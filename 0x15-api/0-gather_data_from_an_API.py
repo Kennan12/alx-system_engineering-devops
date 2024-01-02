4448394889bbcda76b2d6b7f91d03751c78f5b94
kennan12 <kenanusheta24@gmail.com>
2024-01-02 11:13:04 -0800

    0-gather_data_from_an_API.py task 1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
-# using this REST API, for a given employee ID,
-# return information about his/her TODO list progress.
-
-from requests import get
-from sys import argv
-
-def get_data_api(user_id):
-    done = []
-    url = "https://jsonplaceholder.typicode.com/"
-    user = get(url + "users/{}".format(user_id)).json()
-    tasks = get(url + "todos?userId={}".format(user_id)).json()
-    for task in tasks:
-        if task.get("completed") is True:
-            done.append(task.get("title"))
-
-    print("Employee {} is done with tasks({}/{}:"
-            .format(user["name"], len(done), len(tasks)))
-    for task in done:
-        print("\t {}".format(task))
-
+"""
+using this REST API, for a given employee ID,
+Return information about his/her TODO list progress.
+"""
 if __name__ == "__main__":
-    get_data_api(int(argv[1]))
+    import requests
+    import sys
+
+    DONE_TASKS = 0
+    ALL_TASKS = 0
+
+    URL_FOR_USERS = "https://jsonplaceholder.typicode.com/users/{0}".\
+            format(sys.argv[1])
+    URL_FOR_TODOS = "https://jsonplaceholder.typicode.com/todos"
+    R_FOR_USERS = requests.get(URL_FOR_USERS)
+    R_FOR_TODOS = requests.get(URL_FOR_TODOS)
+
+    user = R_FOR_USERS.json().get("user")
+    todos = R_FOR_TODOS.json()
+    for todo in todos:
+        if todo.get("userId") == int(sys.argv[1]):
+            ALL_TASKS += 1
+        if (todo.get("userId") == int(sys.argv[1]))\
+                and (todo.get("completed")):
+            DONE_TASKS += 1
+    print("Employee {} is done with tasks({}/{}:".
+            format(user, DONE_TASKS, ALL_TASKS))
+    for todo in todos:
+        if (todo.get("userId") == int(sys.argv[1]))\
+                and (todo.get('completed')):
+            print("\t {}".format(todo.get('title')))
