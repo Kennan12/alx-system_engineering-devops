4092063b993cd7acfa482166ebc3150c7b63e0e9
kennan12 <kenanusheta24@gmail.com>
2024-01-16 11:48:29 -0800

    api-devops

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,10 +9,10 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the number subscribers"""
     URL = "https://www.reddit.com/r/{}/about/.json". format(subreddit)
-    header = {'User-Agent': 'Chrome/51.0.2704.103'}
-    r= requests.get(URL, headers=headers)
+    headers = {'User-Agent': 'Chrome/51.0.2704.103'}
+    r = requests.get(URL, headers=headers)
     if r.status_code == 200:
-        r= r.json()
+        r = r.json()
         return r['data']['subscribers']
     else:
         return 0
