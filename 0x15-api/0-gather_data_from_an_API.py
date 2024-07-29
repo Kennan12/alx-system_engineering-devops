86b855937a28c368f2f77400faa19af81fc50309
Kennan12 <kenanusheta24@gmail.com>
2024-07-29 13:10:48 -0700

    task-00

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,24 +4,26 @@ For a given employee ID, returns information about his/her
 '''
 
 if __name__ == '__main__':
+    
     import requests
     import sys
-
+    import json
+    
     NUMBER_OF_DONE_TASKS = 0
     TASK_TITLE = []
     USER_ID = sys.argv[1]
     req = requests.get('https://jsonplaceholder.typicode.com/user/{}'.
-			format(USER_ID)).json()
+                       format(USER_ID)).json()
     USERNAME = req.get("username")
     req = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
-			format(USER_ID)).json()
+                       format(USER_ID)).json()
     for item in req:
         if item.get('completed') is True:
             TASK_TITLE.append(item.get('title'))
-            NUMBER_OF_DONE_TASK += 1
-    TOTAL_NUMBER_OF_TASKS = len(req)
+            NUMBER_OF_DONE_TASKS += 1
+            TOTAL_NUMBER_OF_TASKS = len(req)
 
     print('Employee {} is done with tasks{}/{}:'.
-	  format(EMPLOYE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+          format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
     for title in TASK_TITLE:
         print('\t {}'.format(title))
